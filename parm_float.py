1fcdc42323f989574218c12a0ff6ba350dd63918
root <root@centos7.ohana.org.br>
2019-06-24 19:13:04 -0300

    Parãmetros posicionais em Python - float

---
+++ parm_float.py
@@ -0,0 +1,13 @@
+
+# Parâmetros posicionais em Python
+# O equivalente aos $1, $2, $3, $n em
+# shell script
+#
+#
+# Exemplo: parm_float.py 5.678658
+# A Saída será:
+#
+#5.678658 É um float
+import sys
+nn=float(sys.argv[1])
+print (nn, "É um float")
