a8a499a873431b6e98c5ed4cb62a5ca42ee969f2
root <root@centos7.ohana.org.br>
2019-06-27 10:41:05 -0300

    Educação financeira básica em Python

--- hp12c.py
+++ hp12c.py
@@ -38,6 +38,45 @@ print (((1+(6.4/100)) ** (1/12) - 1) * 100)
 # Lhe ofereceu uma módica taxa de "apenas" 15% ao mês...
 # Olhe o tamanho da lombriga efetiva anual!
 
-print ((100 * ((1+(15/100)) ** 12) / 100 -1) * 100)
+print ((100 * ((1+(8/100)) ** 12) / 100 -1) * 100)
 
 # Pretendo fazer mais um appends nesse arquivo portanto fiquem atentos!
+
+# ===========================================
+
+# Série de pagamentos uniforme mais conhecido como carnê no mei tempo e atualmente como boleto. É o método
+# Para calcular valores relativos a financiamentos, prestações etc. Neste exemplo estamos tratando da 
+# série postecipada que nas calculadoras financeiras são conhecidas como BGN/END (Begin e End)
+# Digamos que você resolve comprar um carro velho para passear com a família fim de semana ou rodar
+# no Uber, 99 e coisas do gênero em virtude de estar desempregado. A fubica custa R$ 15.000,00 a vista
+# a taxa é de 2% ao mês, e você vai financia-lo sem entrada em 24 meses. Qual o valor da prestação?
+
+print ((1 + (2/100)) ** 24 * (2/100) / ((1 + (2/100)) ** 24 - 1) * 15000)
+
+# Agora vamos fazer a operação inversa, ou seja Dada a taxa de juros, o prazo e o valor da prestação você quer saber
+# qual é o valor financiado.
+
+print (793.06 / ((1+(2/100)) ** 24 * (2/100) / ((1+(2/100)) ** 24 - 1)))
+
+# Para a mesma operação acima agora queremos calcular o número de períodos dadas a taxa de juros de 2% ao mês
+# o valor financiado de R$ 15.000,00 e a prestação de 793.06. Nesse caso usaremos o módulo math do Python
+# para calcular-mos o ln (logarítimo natural)
+
+def periods():
+    import math
+    print (math.log(1 - ((15000/793.06) * (2/100))) / math.log(1 + (2/100)) * -1)
+
+periods()
+
+# Notem que importei o módulo math "por dentro da função" periods alguns acham isso gambiarra,
+# mas tem muitas utilidades dependendo do que estiver-mos implementando. Fica a caráter de ilustração.
+
+
+# Para o caso de tratar-se de uma série de pagamentos antecipada o cálculo é o mesmo bastando para isso deduzir
+# o valor da entrada do valor financiado que no caso é R$ 15.000,00 e diminuir em 1 o prazo de financiamento
+# ou seja 23 meses.
+
+# Agora vamos dar outro exemplo, você quer comprar uma moto velha para ir trabalhar visto que o transporte
+# público é uma merda mas você só pode arcar com uma prestação de no máximo R$ 500,00 por mês e você deseja
+# saber qual a taxa que você ira pagar.
+
