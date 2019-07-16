a51b222ffda947dda8bcb18ee06bd38aa64f7e5e
root <root@centos7.ohana.org.br>
2019-07-16 05:16:55 -0300

    Insere texto com caixa de fundo

---
+++ Insertion_Text_in_image_backgrondbox.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+Sample='texto de teste'
+
+img = Image.open("sample_in.jpg")
+draw = ImageDraw.Draw(img)
+# font = ImageFont.truetype(<font-file>, <font-size>)
+font = ImageFont.truetype("DejaVuSansMono.ttf", 12)
+# draw.text((x, y),"Sample Text",(r,g,b))
+draw.rectangle(xy=[(5,5), (100,20)], fill=(255,255,120))
+draw.text((5, 5), Sample, (100,255,0), font=font)
+img.save('sample-out.jpg')
