01d4c8ba513443013ed54553044905f6158e3c2c
root <root@centos7.ohana.org.br>
2019-09-25 15:55:17 -0300

    Especificando delay em um programa em Python.

---
+++ sleep.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6                                                                                                                                                     
+# -*- coding: utf-8 -*
+import time
+
+print("Teste 001")
+#Define o tempo em segundos em decimais. Ex 0.5 são 500ms, 10 são 10 segundos.
+time.sleep(1)
+print("Teste 002")
