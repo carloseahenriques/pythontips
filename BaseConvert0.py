6ef32844c1515e2da1405bf477ce0013c085927b
root <root@centos7.ohana.org.br>
2019-10-01 09:55:40 -0300

    Conversão de bases

---
+++ BaseConvert0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*
+
+# Converte o número 253 base 10 para Hexadecimal (maiúsculo)
+print("{0:X}".format(253))
+
+# Converte o número 253 base 10 para Hexadecimal (minúsculo)
+print("{0:x}".format(253))
+
+#Converte hexadecimal para decimal
+print("{0:10}".format(0x2fd4))
+
+#Converte hexadecimal para binário
+print ("{0:b}".format(0xdf))
