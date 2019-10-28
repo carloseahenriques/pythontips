78d9dbb88998dafd583788e4d8d2cbfac222b812
root <root@centos7.ohana.org.br>
2019-10-28 16:05:44 -0300

    Lista números primos em um dado intervalo.

--- PrimeNumber_List.py
+++ PrimeNumber_List.py
@@ -5,14 +5,7 @@ import sys
 
 start=int(sys.argv[1])
 end=int(sys.argv[2])
-'''
-for counter in range(2,n):
-    if (n % counter == 0):
-	    print("Multiplo de", counter)
-	    mult +=1
-if(mult==0):
-    print (n, "é PRIMO")
-'''
+
 for counter in range(start, end+1):
 	if counter > 1:
 		for n in range(2, counter):
